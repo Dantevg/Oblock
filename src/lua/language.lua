72876ed0ae05673ee44a2f754f1d721c9064a246
RedPolygon <dantevg006@gmail.com>
2021-06-15 15:05:46 +0200

    Add demo mode

--- src/lua/language.lua
+++ src/lua/language.lua
@@ -2,10 +2,15 @@ local Lexer = require "Lexer"
 local Parser = require "Parser"
 local Interpreter = require "Interpreter"
 
-local path = (...)
+local args = {...}
+local path = args[1]
+local isDemo = args[1] == "--demo" or args[2] == "--demo"
 local file = path and io.open(path) or io.stdin
+
+if isDemo then print() end
 local content = file:read("a")
 file:close()
+if isDemo then print() end
 
 local filename = string.match(path or "", "/([^/]+)$") or path
 
@@ -14,4 +19,9 @@ if not tokens then return end
 local program = Parser(tokens, filename):parse()
 if not program then return end
 
-print(Interpreter(program):interpret())
+if isDemo then
+	Interpreter(program):interpret()
+	print()
+else
+	print(Interpreter(program):interpret())
+end
